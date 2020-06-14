b48e98cb7fdaca43a9da8481a9672b22bba4b417
chaoleng <lengchao000@outlook.com>
2020-06-14 13:49:08 +0000

    add setData

--- src/components/CiList/index.vue
+++ src/components/CiList/index.vue
@@ -1,7 +1,9 @@
 <template>
-	<div class="cinema_body">
+    <div class="cinema_body">
+        <!-- <Loading v-if="isLoading" />
+        <Scroller v-else> -->
         <ul>
-            <li>
+            <!-- <li>
                 <div>
                     <span>大地影院(澳东世纪店)</span>
                     <span class="q"><span class="price">22.9</span> 元起</span>
@@ -14,13 +16,80 @@
                     <div>小吃</div>
                     <div>折扣卡</div>
                 </div>
+            </li> -->
+            <li v-for="item in cinemaList" :key="item.id">
+                <div>
+                    <span>{{ item.nm }}</span>
+                    <span class="q"><span class="price">{{ item.sellPrice }}</span> 元起</span>
+                </div>
+                <div class="address">
+                    <span>{{ item.addr }}</span>
+                    <span>{{ item.distance }}</span>
+                </div>
+                <div class="card">
+                    <div v-for="(num,key) in item.tag" v-if="num===1" :key="key" :class=" key | classCard ">{{ key | formatCard }}</div>
+                </div>
             </li>
         </ul>
+        <!-- </Scroller> -->
     </div>
 </template>
+
 <script>
-export default{
-	name : 'CiList'
+export default {
+    name : 'CiList',
+    data(){
+        return {
+            cinemaList : [],
+            // isLoading : true,
+            // prevCityId : -1
+        };
+    },
+    mounted(){
+
+        // var cityId = this.$store.state.city.id;
+        // if( this.prevCityId === cityId ){ return; }
+        // this.isLoading = true;
+
+        this.axios.get('/api/cinemaList?cityId=10').then((res)=>{
+            var msg = res.data.msg;
+            if(msg === 'ok'){
+                this.cinemaList = res.data.data.cinemas;
+                // this.isLoading = false;
+                // this.prevCityId = cityId
+            }
+        });
+    },
+    filters : {
+        formatCard(key){
+            var card = [
+                { key : 'allowRefund' , value : '改签' },
+                { key : 'endorse' , value : '退' },
+                { key : 'sell' , value : '折扣卡' },
+                { key : 'snack' , value : '小吃'}
+            ];
+            for(var i=0;i<card.length;i++){
+                if(card[i].key === key){
+                    return card[i].value;
+                }
+            }
+            return '';
+        },
+        classCard(key){
+            var card = [
+                { key : 'allowRefund' , value : 'bl' },
+                { key : 'endorse' , value : 'bl' },
+                { key : 'sell' , value : 'or' },
+                { key : 'snack' , value : 'or'}
+            ];
+            for(var i=0;i<card.length;i++){
+                if(card[i].key === key){
+                    return card[i].value;
+                }
+            }
+            return '';
+        }
+    }
 }
 </script>
 
--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -9,13 +9,23 @@
         <div class="search_result">
             <h3>电影/电视剧/综艺</h3>
             <ul>
-                <li>
+                <!-- <li>
                     <div class="img"><img src="/images/movie_1.jpg"></div>
                     <div class="info">
                         <p><span>无名之辈</span><span>8.5</span></p>
                         <p>A Cool Fish</p>
                         <p>剧情,喜剧,犯罪</p>
                         <p>2018-11-16</p>
+                    </div>
+                </li> -->
+                <li v-for="item in moviesList" :key="item.id">
+                    <!-- <div class="img"><img :src=" item.img | setWH('128.180')"></div> -->
+                    <div class="img"><img :src=" item.img | setWH('128.180') "></div>
+                    <div class="info">
+                        <p><span>{{ item.nm }}</span><span>{{ item.sc }}</span></p>
+                        <p>{{ item.enm }}</p>
+                        <p>{{ item.cat }}</p>
+                        <p>{{ item.rt }}</p>
                     </div>
                 </li>
             </ul>
@@ -25,7 +35,48 @@
 
 <script>
 export default{
-	name : 'Search'
+	name : 'Search',
+    data(){
+        return {
+            message : [],
+            moviesList : []
+        }
+    },
+    methods : {
+        cancelrequest(){
+            if(typeof this.source === 'function'){
+                this.source('终止请求');
+
+            }
+        }
+    },
+    watch : {
+        message(newVal){
+            // axios终止多次请求
+            var that = this;
+            this.cancelrequest();
+
+            this.axios.get('/api/searchList?cityId=10&kw=' + newVal,{
+                cancelToken : new this.axios.CancelToken(function(c) {
+                    that.source = c;
+                })
+            }).then((res)=>{
+                var msg = res.data.msg;
+                var movies = res.data.data.movies;
+                if(msg && movies){
+                    this.moviesList = res.data.data.movies.list;
+                }
+            }).catch((err)=>{
+                if(this.axios.isCancel(err)) {
+                    console.log('Rqquest cancel', err.message); //请求如果被取消返回取消的message
+                }
+                else{
+                    //handle erroe
+                    console.log(err);
+                }
+            });
+        }
+    }
 }
 </script>
 
